f3df51d2c9daa6301e07a35b46b5e8057118f09a
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-18 03:35:26 +0900

    feat : 가짜 데이터 사용해보기

--- ch01_5/src/App.tsx
+++ ch01_5/src/App.tsx
@@ -1,26 +1,14 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+// 48p 가짜 데이터 사용해 보기
+import * as D from './data'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        {D.randomName()}, {D.randomJobTitle()}, {D.randomDayMonthYear()}
+      </p>
+      <img src={D.randomAvatar()} alt="" />
+      <img src={D.randomImage()} alt="" />
     </div>
-  );
+  )
 }
-
-export default App;
