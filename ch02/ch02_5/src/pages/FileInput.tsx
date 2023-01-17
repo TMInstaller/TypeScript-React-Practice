58dbe738594c41f92db46ee2a0c9d206b7b940c1
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-18 05:35:31 +0900

    feat : FileInput 작성

--- ch02/ch02_5/src/pages/FileInput.tsx
+++ ch02/ch02_5/src/pages/FileInput.tsx
@@ -1,4 +1,20 @@
-// 99p 복사용 기본 코드 작성
-export default function CopyMe() {
-  return <div>CopyMe</div>
+// 120p 파일 입력
+import type { ChangeEvent } from 'react'
+
+export default function FileInput() {
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const files: FileList | null = e.target.files
+    if (files) {
+      for (let i = 0; i < files.length; i++) {
+        const file: File | null = files.item(i)
+        console.log(`file[${i}]:`, file)
+      }
+    }
+  }
+  return (
+    <div>
+      <p>FileInput</p>
+      <input type="file" onChange={onChange} multiple accept="image/*" />
+    </div>
+  )
 }
