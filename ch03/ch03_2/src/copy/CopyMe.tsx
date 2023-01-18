d8fc770e295ef549343735d4eabdfb69916acb34
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-18 14:28:01 +0900

    Revert "feat : CopyMe 컴포넌트형으로 변경"
    This reverts commit 3ac9ddab112f5cfdb6f40ba97756febb0ca4db4e.

--- ch03/ch03_2/src/copy/CopyMe.tsx
+++ ch03/ch03_2/src/copy/CopyMe.tsx
@@ -1,11 +1,4 @@
-// 167p Title 컴포넌트를 사용하는 기본 파일 만들기
-import { Title } from '../components'
-
+// 99p 복사용 기본 코드 작성
 export default function CopyMe() {
-  return (
-    <section className="mt-4">
-      <Title>CopyMe</Title>
-      <div className="mt-4"></div>
-    </section>
-  )
+  return <div>CopyMe</div>
 }
