6425fe72cbceb8fc788086c54baa51d7a2aabcd1
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-18 10:49:19 +0900

    design : Style.tsx

--- ch03/ch03_1/src/pages/Style.tsx
+++ ch03/ch03_1/src/pages/Style.tsx
@@ -1,4 +1,17 @@
-// 99p 복사용 기본 코드 작성
-export default function CopyMe() {
-  return <div>CopyMe</div>
+// 138p 스타일 객체 설정하기
+export default function Style() {
+  return (
+    <div>
+      <h3>Style</h3>
+      <span className="material-icons" style={{ color: 'blue' }}>
+        home
+      </span>
+      <span
+        className="material-icons"
+        style={{ fontSize: '50px', color: 'red' }}
+      >
+        check_circle_outline
+      </span>
+    </div>
+  )
 }
