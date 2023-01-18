9dd7827ae67f4cd52f009119d46f8672392ff3f1
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-18 17:37:57 +0900

    design : DisplayNoneTest

--- ch03/ch03_3/src/pages/DisplayNoneTest.tsx
+++ ch03/ch03_3/src/pages/DisplayNoneTest.tsx
@@ -1,11 +1,15 @@
 // 167p Title 컴포넌트를 사용하는 기본 파일 만들기
 import { Title } from '../components'
 
-export default function CopyMe() {
+export default function DisplayNoneTest() {
   return (
     <section className="mt-4">
-      <Title>CopyMe</Title>
-      <div className="mt-4"></div>
+      <Title>DisplayNoneTest</Title>
+      <div className="mt-4">
+        <p className="visible">visibility: visible text</p>
+        <p className="invisible">visibility: hidden text</p>
+        <p className="hidden">visibility: none text</p>
+      </div>
     </section>
   )
 }
