aaea4bca3e3f82184249c55bff4630367817a443
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-18 14:52:25 +0900

    design : DivTest

--- ch03/ch03_3/src/pages/DivTest.tsx
+++ ch03/ch03_3/src/pages/DivTest.tsx
@@ -1,11 +1,15 @@
 // 167p Title 컴포넌트를 사용하는 기본 파일 만들기
-import { Title } from '../components'
+// 171p Div 컴포넌트 사용하기
+import { Title, Subtitle, Div, Icon } from '../components'
 
-export default function CopyMe() {
+export default function DivTest() {
   return (
     <section className="mt-4">
-      <Title>CopyMe</Title>
-      <div className="mt-4"></div>
+      <Title>DivTest</Title>
+      <Div className="text-center text-blue-100 bg-blue-600 " height="6rem">
+        <Icon name="home" className="text-3xl " />
+        <Subtitle>Home</Subtitle>
+      </Div>
     </section>
   )
 }
