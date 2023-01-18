18336ffbfeabf3d1c0dc91683e50ecdfc540dd36
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-18 14:33:21 +0900

    rename : 실습용 파일 생성

---
+++ ch03/ch03_3/src/pages/HeightTest.tsx
@@ -0,0 +1,11 @@
+// 167p Title 컴포넌트를 사용하는 기본 파일 만들기
+import { Title } from '../components'
+
+export default function CopyMe() {
+  return (
+    <section className="mt-4">
+      <Title>CopyMe</Title>
+      <div className="mt-4"></div>
+    </section>
+  )
+}
