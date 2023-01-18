da76241dee336c4169d654ba8dbcf3d7f417520b
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-18 11:25:46 +0900

    feat : ch03_1 App.tsx

--- ch03/ch03_2/src/App.tsx
+++ ch03/ch03_2/src/App.tsx
@@ -1,18 +1,14 @@
 // 128p 기본 앱  파일
-import Bootstrap from './pages/Bootstrap'
-import Icon from './pages/Icon'
-import Style from './pages/Style'
-import UsingIcon from './pages/UsingIcon'
-import UsingIconWithCSSClass from './pages/UsingIconWithCSSClass'
+import Tailwindcss from './pages/Tailwindcss'
+import Color from './pages/Color'
+import TextTest from './pages/TextTest'
 
 export default function App() {
   return (
     <div>
-      <UsingIconWithCSSClass />
-      <UsingIcon />
-      <Style />
-      <Icon />
-      <Bootstrap />
+      <TextTest />
+      <Color />
+      <Tailwindcss />
     </div>
   )
 }
