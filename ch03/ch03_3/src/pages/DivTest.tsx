35c6ae5143f6f711886fcf2ef8475581019cfc7a
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-18 14:54:33 +0900

    design : DivTest 수정

--- ch03/ch03_3/src/pages/DivTest.tsx
+++ ch03/ch03_3/src/pages/DivTest.tsx
@@ -1,12 +1,13 @@
-// 167p Title 컴포넌트를 사용하는 기본 파일 만들기
 // 171p Div 컴포넌트 사용하기
+// 173p 웹 브라우저의 높이 결정 메커니즘 적용하기
 import { Title, Subtitle, Div, Icon } from '../components'
 
 export default function DivTest() {
   return (
     <section className="mt-4">
       <Title>DivTest</Title>
-      <Div className="text-center text-blue-100 bg-blue-600 " height="6rem">
+      {/* <Div className="text-center text-blue-100 bg-blue-600 " height="6rem"> */}
+      <Div className="text-center text-blue-100 bg-blue-600 ">
         <Icon name="home" className="text-3xl " />
         <Subtitle>Home</Subtitle>
       </Div>
