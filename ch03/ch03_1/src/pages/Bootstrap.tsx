247a2933c90c52ed80125591da13d182dc2460de
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-18 10:34:10 +0900

    feat : Bootstrap.tsx

--- ch03/ch03_1/src/pages/Bootstrap.tsx
+++ ch03/ch03_1/src/pages/Bootstrap.tsx
@@ -1,4 +1,44 @@
-// 99p 복사용 기본 코드 작성
+// 130p 오류가 있는 부트스트랩 스타일링 코드
 export default function CopyMe() {
-  return <div>CopyMe</div>
+  return (
+    <form>
+      <div className="mb-3">
+        <label htmlFor="exampleInputEmail1" className="form-label">
+          Email address
+        </label>
+        <input
+          type="email"
+          className="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+        />
+        <div id="emailHelp" className="form-text">
+          We'll never share your email with anyone else.
+        </div>
+      </div>
+      <div className="mb-3">
+        <label htmlFor="exampleInputPassword1" className="form-label">
+          Password
+        </label>
+        <input
+          type="password"
+          className="form-control"
+          id="exampleInputPassword1"
+        />
+      </div>
+      <div className="mb-3 form-check">
+        <input
+          type="checkbox"
+          className="form-check-input"
+          id="exampleCheck1"
+        />
+        <label className="form-check-label" htmlFor="exampleCheck1">
+          Check me out
+        </label>
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  )
 }
