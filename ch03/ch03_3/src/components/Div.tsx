c2bf67524dc4ae2318caa9b064ebf3822377b66f
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-18 17:44:01 +0900

    design : Div방향값 추가

--- ch03/ch03_3/src/components/Div.tsx
+++ ch03/ch03_3/src/components/Div.tsx
@@ -1,5 +1,6 @@
 // 171p WidthHeight 타입 적용하기
 // 184p Div component src 속성 추가
+// 197p 위치 타입과 속성 추가하기
 import type {
   FC,
   DetailedHTMLProps,
@@ -7,13 +8,16 @@ import type {
   PropsWithChildren,
 } from 'react'
 import { WidthHeight } from './WidthHeight'
+import type { LeftRightTopBottom } from './LeftRightTopBottom'
 
 export type ReactDivProps = DetailedHTMLProps<
   HTMLAttributes<HTMLDivElement>,
   HTMLDivElement
 >
 export type DivProps = ReactDivProps &
-  PropsWithChildren<WidthHeight> & { src?: string }
+  PropsWithChildren<WidthHeight> & { src?: string } & LeftRightTopBottom & {
+    src?: string
+  }
 
 export const Div: FC<DivProps> = ({
   width,
@@ -21,6 +25,10 @@ export const Div: FC<DivProps> = ({
   style: _style,
   src,
   className: _className,
+  left,
+  right,
+  top,
+  bottom,
   ...props
 }) => {
   const style = {
@@ -28,6 +36,10 @@ export const Div: FC<DivProps> = ({
     width,
     height,
     backgroundImage: src && `url(${src})`,
+    left,
+    right,
+    top,
+    bottom,
   }
   const className = ['box-sizing', src && 'bg-gray-300', _className].join('')
   return <div {...props} className={className} style={style} />
