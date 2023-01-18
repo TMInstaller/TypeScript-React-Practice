47b5c5ce3fbfc91e1fcad6d853c9e5df19a394ae
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-18 16:36:32 +0900

    design : marginTest, imageTest

--- ch03/ch03_3/src/pages/ImageTest.tsx
+++ ch03/ch03_3/src/pages/ImageTest.tsx
@@ -1,11 +1,13 @@
-// 167p Title 컴포넌트를 사용하는 기본 파일 만들기
+// 182p margin style 속성 테스트
 import { Title } from '../components'
+import * as D from '../data'
 
-export default function CopyMe() {
+const src = D.randomImage(3000, 1600)
+export default function ImageTest() {
   return (
     <section className="mt-4">
-      <Title>CopyMe</Title>
-      <div className="mt-4"></div>
+      <Title>ImageTest</Title>
+      <img src={src} className="bg-gray-300 " alt="" width="400" height="400" />
     </section>
   )
 }
--- ch03/ch03_3/src/pages/MarginTest.tsx
+++ ch03/ch03_3/src/pages/MarginTest.tsx
@@ -2,12 +2,15 @@
 import { Title } from '../components'
 import * as D from '../data'
 
-const src = D.randomImage(300, 600)
-export default function ImageTest() {
+export default function MarginTest() {
+  const boxes = D.makeArray(10).map((notUsed, index) => (
+    <div key={index} className="inline-block w-8 h-8 m-4 bg-pink-300" />
+  ))
+
   return (
     <section className="mt-4">
-      <Title>ImageTest</Title>
-      <img src={src} className="bg-gray-300 w-[400px] h-[400px] " alt="" />
+      <Title>MarginTest</Title>
+      <div className="p-4 bg-blue-700 ">{boxes}</div>
     </section>
   )
 }
