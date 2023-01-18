fafe05023cba5f601adb3850117bd7221771de76
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-18 14:36:35 +0900

    feat : App.tsx에 띄워두기

--- ch03/ch03_3/src/App.tsx
+++ ch03/ch03_3/src/App.tsx
@@ -1,14 +1,32 @@
-// 149p 기본 앱  파일
-import Tailwindcss from './pages/Tailwindcss'
-import Color from './pages/Color'
-import TextsTest from './pages/TextsTest'
+// 168p 기본 앱 파일
+import AvatarTest from './pages/AvatarTest'
+import BackgroundImageTest from './pages/BackgroundImageTest'
+import DisplayNoneTest from './pages/DisplayNoneTest'
+import DisplayTest from './pages/DisplayTest'
+import DivTest from './pages/DivTest'
+import HeightTest from './pages/HeightTest'
+import ImageTest from './pages/ImageTest'
+import MarginTest from './pages/MarginTest'
+import OverlayTest from './pages/OverlayTest'
+import PaddingTest from './pages/PaddingTest'
+import PositionTest from './pages/PositionTest'
+import ViewportTest from './pages/ViewportTest'
 
 export default function App() {
   return (
-    <div>
-      <TextsTest />
-      <Color />
-      <Tailwindcss />
-    </div>
+    <main>
+      <OverlayTest />
+      <PositionTest />
+      <DisplayNoneTest />
+      <DisplayTest />
+      <AvatarTest />
+      <BackgroundImageTest />
+      <ImageTest />
+      <MarginTest />
+      <PaddingTest />
+      <HeightTest />
+      <ViewportTest />
+      <DivTest />
+    </main>
   )
 }
