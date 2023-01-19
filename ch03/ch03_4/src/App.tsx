02e0b613875007ce5bdcd04ed1e7b32fea7c4a81
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-19 13:08:43 +0900

    feat : 기본 앱 파일 추가

--- ch03/ch03_4/src/App.tsx
+++ ch03/ch03_4/src/App.tsx
@@ -1,32 +1,18 @@
-// 168p 기본 앱 파일
-import AvatarTest from './pages/AvatarTest'
-import BackgroundImageTest from './pages/BackgroundImageTest'
-import DisplayNoneTest from './pages/DisplayNoneTest'
-import DisplayTest from './pages/DisplayTest'
-import DivTest from './pages/DivTest'
-import HeightTest from './pages/HeightTest'
-import ImageTest from './pages/ImageTest'
-import MarginTest from './pages/MarginTest'
-import OverlayTest from './pages/OverlayTest'
-import PaddingTest from './pages/PaddingTest'
-import PositionTest from './pages/PositionTest'
-import ViewportTest from './pages/ViewportTest'
+// 204p 기본 앱 파일
+import AlignTest from './pages/AlignTest'
+import CardContainer from './pages/CardContainer'
+import JustifyCenterTest from './pages/JustifyCenterTest'
+import MinMaxTest from './pages/MinMaxTest'
+import UserContainer from './pages/UserContainer'
 
 export default function App() {
   return (
     <main>
-      <OverlayTest />
-      <PositionTest />
-      <DisplayNoneTest />
-      <DisplayTest />
-      <AvatarTest />
-      <BackgroundImageTest />
-      <ImageTest />
-      <MarginTest />
-      <PaddingTest />
-      <HeightTest />
-      <ViewportTest />
-      <DivTest />
+      <CardContainer />
+      <UserContainer />
+      <AlignTest />
+      <JustifyCenterTest />
+      <MinMaxTest />
     </main>
   )
 }
