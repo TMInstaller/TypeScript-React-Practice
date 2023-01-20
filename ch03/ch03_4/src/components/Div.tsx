ab24afc2f8f0aa99eb872a7856adf5b22e39c6a2
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-20 21:25:52 +0900

    feat : MinMaxHeightWidth 적용하기

--- ch03/ch03_4/src/components/Div.tsx
+++ ch03/ch03_4/src/components/Div.tsx
@@ -1,45 +1,39 @@
 // 171p WidthHeight 타입 적용하기
 // 184p Div component src 속성 추가
 // 197p 위치 타입과 속성 추가하기
-import type {
-  FC,
-  DetailedHTMLProps,
-  HTMLAttributes,
-  PropsWithChildren,
-} from 'react'
+// 212p MinMaxWidthHeight 타입 반영하기
+// prettier-ignore
+import type { FC, DetailedHTMLProps, HTMLAttributes, PropsWithChildren } from 'react'
 import { WidthHeight } from './WidthHeight'
 import type { LeftRightTopBottom } from './LeftRightTopBottom'
+import type { MinMaxWidthHeight } from './MinMaxWidthHeight'
 
 export type ReactDivProps = DetailedHTMLProps<
   HTMLAttributes<HTMLDivElement>,
   HTMLDivElement
 >
 export type DivProps = ReactDivProps &
-  PropsWithChildren<WidthHeight> & { src?: string } & LeftRightTopBottom & {
+  PropsWithChildren<WidthHeight> &
+  LeftRightTopBottom &
+  MinMaxWidthHeight & {
     src?: string
   }
-
+// prettier-ignore
 export const Div: FC<DivProps> = ({
-  width,
-  height,
+  width, height,
   style: _style,
   src,
   className: _className,
-  left,
-  right,
-  top,
-  bottom,
+  left, right, top, bottom,
+  minWidth, maxWidth, minHeight, maxHeight,
   ...props
 }) => {
   const style = {
     ..._style,
-    width,
-    height,
+    width, height,
     backgroundImage: src && `url(${src})`,
-    left,
-    right,
-    top,
-    bottom,
+    left, right, top, bottom,
+    minWidth, maxWidth, minHeight, maxHeight
   }
   const className = ['box-sizing', src && 'bg-gray-300', _className].join('')
   return <div {...props} className={className} style={style} />
