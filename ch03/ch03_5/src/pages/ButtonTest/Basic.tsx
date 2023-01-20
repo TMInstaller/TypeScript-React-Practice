5afefb7b02d7bf276cf123f01059aebd30c624c3
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-20 23:43:19 +0900

    feat : daisyui button

--- ch03/ch03_5/src/pages/ButtonTest/Basic.tsx
+++ ch03/ch03_5/src/pages/ButtonTest/Basic.tsx
@@ -1,8 +1,12 @@
-// 230p 상대 경로 문제 해결하기
-export default function CopyMe() {
+// 234p 버튼 테스트 코드 구현하기
+export default function Basic() {
   return (
     <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center ">CopyMe</h2>
+      <h2 className="text-3xl font-bold text-center ">Basic</h2>
+      <div className="flex mt-4 justify-evenly">
+        <button className="btn btn-primary">daisyui button</button>
+        <button className="btn btn-primary">Button</button>
+      </div>
       <div className="mt-4"></div>
     </section>
   )
--- ch03/ch03_5/src/theme/daisyui/Button.tsx
+++ ch03/ch03_5/src/theme/daisyui/Button.tsx
@@ -1,4 +1,5 @@
 // 234p 버튼 컴포넌트 구현하기
+// 236p btn 클래스 기본으로 반영하기
 // prettier-ignore
 import type { FC, DetailedHTMLProps, ButtonHTMLAttributes, PropsWithChildren } from "react";
 
@@ -10,7 +11,9 @@ export type ReactButtonProps = DetailedHTMLProps<
 export type ButtonProps = ReactButtonProps & {}
 
 export const Button: FC<PropsWithChildren<ButtonProps>> = ({
+  className: _className,
   ...buttonProps
 }) => {
-  return <button {...buttonProps} />
+  const className = ['btn', _className].join(' ')
+  return <button {...buttonProps} className={className} />
 }
