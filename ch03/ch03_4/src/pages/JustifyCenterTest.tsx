dfbdccbe32d88918d36fcaa819e774717e3cf910
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-20 21:38:45 +0900

    feat : JustifyCenter

--- ch03/ch03_4/src/pages/JustifyCenterTest.tsx
+++ ch03/ch03_4/src/pages/JustifyCenterTest.tsx
@@ -1,11 +1,24 @@
-// 167p Title 컴포넌트를 사용하는 기본 파일 만들기
-import { Title } from '../components'
+// 215p 플렉스 컨테이너의 콘텐츠 조정 실습
+import { Div, Title, Subtitle } from '../components'
+import * as D from '../data'
 
-export default function CopyMe() {
+export default function JustifyCenterTest() {
+  const boxes = D.range(0, 5).map((index) => (
+    <Div key={index} className="w-4 h-4 m-1 bg-black" minHeight="auto" />
+  ))
   return (
     <section className="mt-4">
-      <Title>CopyMe</Title>
-      <div className="mt-4"></div>
+      <Title>JustifyCenterTest</Title>
+      <div className="mt-4">
+        <Subtitle>flex flex-row justify-center</Subtitle>
+        <div className="flex flex-row justify-center h-40 bg-gray-300">
+          {boxes}
+        </div>
+        <Subtitle>flex flex-col justify-center</Subtitle>
+        <div className="flex flex-col justify-center h-40 bg-gray-300">
+          {boxes}
+        </div>
+      </div>
     </section>
   )
 }
