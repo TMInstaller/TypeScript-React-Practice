37c8be4842ccfffedd19b6b8201d0e0f986e449d
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-21 22:39:52 +0900

    feat : callback

--- ch04/ch04_2/src/pages/Callback.tsx
+++ ch04/ch04_2/src/pages/Callback.tsx
@@ -1,9 +1,32 @@
-// 230p 상대 경로 문제 해결하기
-export default function CopyMe() {
+// 279p useCallback 훅으로 onClick 콜백 호출하기
+import { useMemo, useCallback } from 'react'
+import { Title } from '../components'
+import { Button } from '../theme/daisyui'
+import * as D from '../data'
+
+export default function Callback() {
+  const onClick = useCallback(() => alert('button clicked'), [])
+
+  const buttons = useMemo(
+    () =>
+      D.makeArray(3)
+        .map(D.randomName)
+        .map((name, index) => (
+          <Button
+            key={index}
+            onClick={onClick}
+            className="normal-case btn btn-primary btn-wide btn-xs"
+          >
+            {name}
+          </Button>
+        )),
+    [onClick]
+  )
+
   return (
     <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center ">CopyMe</h2>
-      <div className="mt-4"></div>
+      <Title>Callback</Title>
+      <div className="flex mt-4 justify-evenly">{buttons}</div>
     </section>
   )
 }
