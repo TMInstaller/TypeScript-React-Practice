15910db455abebf676f98b4a45800ac15660b9ef
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-21 17:33:34 +0900

    feat : input color

--- ch03/ch03_5/src/pages/InputTest/Color.tsx
+++ ch03/ch03_5/src/pages/InputTest/Color.tsx
@@ -1,9 +1,40 @@
-// 230p 상대 경로 문제 해결하기
-export default function CopyMe() {
+// 242p Input 컴포넌트에 색상 설정하기
+import { Input } from '../../theme/daisyui'
+
+export default function Color() {
   return (
     <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center ">CopyMe</h2>
-      <div className="mt-4"></div>
+      <h2 className="text-3xl font-bold text-center ">Color</h2>
+      <div className="flex flex-col p-4 mt-4 ">
+        <div>
+          <label className="label">input-primary</label>
+          <Input className=" input-primary" />
+        </div>
+        <div>
+          <label className="label">input-secondary</label>
+          <Input className=" input-secondary" />
+        </div>
+        <div>
+          <label className="label">input-accent</label>
+          <Input className=" input-accent" />
+        </div>
+        <div>
+          <label className="label">input-info</label>
+          <Input className=" input-info" />
+        </div>
+        <div>
+          <label className="label">input-success</label>
+          <Input className=" input-success" />
+        </div>
+        <div>
+          <label className="label">input-warning</label>
+          <Input className=" input-warning" />
+        </div>
+        <div>
+          <label className="label">input-error</label>
+          <Input className=" input-error" />
+        </div>
+      </div>
     </section>
   )
 }
