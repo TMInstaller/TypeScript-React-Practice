bc23247df3a54dee6488a8d24209cb2123c81588
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-21 22:03:52 +0900

    feat : 커스텀 훅 웹 시계

--- ch04/ch04_1/src/App.tsx
+++ ch04/ch04_1/src/App.tsx
@@ -1,21 +1,8 @@
-// 254p 기본 앱 파일
-// 260p useEffect 훅 사용 초기 모습
-// 261p 화면 갱신 버그가 있는 버전
-// 262p useRef 훅 사용(여전히 화면은 갱신하지 않음)
-// 263p 시계 앱 완성(useRef 대신 useState 훅 사용)
-import { useEffect, useState } from 'react'
+// 266p useClock 커스텀 훅으로 만든 시계
+import { useClock } from './hooks'
 import Clock from './pages/Clock'
 
 export default function App() {
-  const [today, setToday] = useState(new Date())
-
-  useEffect(() => {
-    console.log('useEffect called.')
-    const duration = 1000
-    const id = setInterval(() => {
-      setToday(new Date())
-    }, duration)
-    return () => clearInterval(id)
-  }, [])
+  const today = useClock()
   return <Clock today={today} />
 }
