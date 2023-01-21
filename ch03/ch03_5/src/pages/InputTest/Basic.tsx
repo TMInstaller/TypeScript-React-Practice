83ff537f2a65c828fefcd3d125e03ab38608b0eb
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-21 17:29:42 +0900

    feat : Basic

--- ch03/ch03_5/src/pages/InputTest/Basic.tsx
+++ ch03/ch03_5/src/pages/InputTest/Basic.tsx
@@ -1,9 +1,14 @@
-// 230p 상대 경로 문제 해결하기
-export default function CopyMe() {
+// 242p Input 컴포넌트 사용하기
+import { Input } from '../../theme/daisyui'
+
+export default function Basic() {
   return (
     <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center ">CopyMe</h2>
-      <div className="mt-4"></div>
+      <h2 className="text-3xl font-bold text-center ">Basic</h2>
+      <div className="mt-4 justify-evenly">
+        <input className="input input-primary" />
+        <Input className="input-primary" />
+      </div>
     </section>
   )
 }
