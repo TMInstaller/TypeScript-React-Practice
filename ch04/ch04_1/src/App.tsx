e10b5a6f6c5569898ea1b0c6a0fe7fb57e3b7b4d
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-21 20:50:18 +0900

    feat : useRef사용해보기

--- ch04/ch04_1/src/App.tsx
+++ ch04/ch04_1/src/App.tsx
@@ -1,19 +1,20 @@
 // 254p 기본 앱 파일
 // 260p useEffect 훅 사용 초기 모습
 // 261p 화면 갱신 버그가 있는 버전
-import { useEffect } from 'react'
+// 262p useRef 훅 사용(여전히 화면은 갱신하지 않음)
+import { useEffect, useRef } from 'react'
 import Clock from './pages/Clock'
 
 export default function App() {
-  let today = new Date()
+  let today = useRef(new Date())
   useEffect(() => {
     console.log('useEffect called.')
     const duration = 1000
     const id = setInterval(() => {
-      today = new Date()
-      console.log('today', today.toLocaleTimeString())
+      today.current = new Date()
+      console.log('today', today.current.toLocaleTimeString())
     }, duration)
     return () => clearInterval(id)
   }, [])
-  return <Clock today={today} />
+  return <Clock today={today.current} />
 }
