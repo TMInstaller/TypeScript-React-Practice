97d8a718e6ac2c298d17e6e442e5841d8c978c42
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-21 22:52:48 +0900

    chore : ch04_2 setting

--- ch04/ch04_3/src/App.tsx
+++ ch04/ch04_3/src/App.tsx
@@ -1,16 +1,25 @@
 // 269p 기본 앱 파일 작성하기
-import Callback from './pages/Callback'
-import HighOrderCallback from './pages/HighOrderCallback'
-import Memo from './pages/Memo'
-import UseOrCreateTest from './pages/UseOrCreateTest'
+
+import ArrayState from './pages/ArrayState'
+import BasicForm from './pages/BasicForm'
+import HighOrderRadioInputTest from './pages/HighOrderRadioInputTest'
+import InputTest from './pages/InputTest'
+import NumberState from './pages/NumberState'
+import ObjectState from './pages/ObjectState'
+import RadioInputTest from './pages/RadioInputTest'
+import ShowHideModal from './pages/ShowHideModal'
 
 export default function App() {
   return (
-    <div>
-      <HighOrderCallback />
-      <Callback />
-      <Memo />
-      <UseOrCreateTest />
-    </div>
+    <main>
+      <ArrayState />
+      <ObjectState />
+      <BasicForm />
+      <HighOrderRadioInputTest />
+      <RadioInputTest />
+      <ShowHideModal />
+      <InputTest />
+      <NumberState />
+    </main>
   )
 }
