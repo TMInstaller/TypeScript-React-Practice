4c24cb5e63a3f65deb3710ade6c5739dd917388c
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-21 23:08:08 +0900

    feat : InputTest

--- ch04/ch04_3/src/pages/InputTest.tsx
+++ ch04/ch04_3/src/pages/InputTest.tsx
@@ -1,9 +1,39 @@
-// 230p 상대 경로 문제 해결하기
-export default function CopyMe() {
+// 289p <input> 요소 테스트(초기 모습)
+// 291p <input> 요소 테스트(완성)
+import type { ChangeEvent } from 'react'
+import { useState, useCallback } from 'react'
+import { Title } from '../components'
+
+export default function InputTest() {
+  const [value, setValue] = useState<string>('')
+  const [checked, setChecked] = useState<boolean>(false)
+
+  const onChangeValue = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    // console.log(e.target.value)
+    setValue((notUsed) => e.target.value)
+  }, [])
+  const onChangeChecked = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    // console.log(e.target.checked)
+    setChecked((notUsed) => e.target.checked)
+  }, [])
+
   return (
     <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center ">CopyMe</h2>
-      <div className="mt-4"></div>
+      <Title>InputTest</Title>
+      <div className="flex items-center justify-center p-4 mt-4 ">
+        <input
+          type="text"
+          value={value}
+          onChange={onChangeValue}
+          className="input-primary input-sm"
+        />
+        <input
+          type="checkbox"
+          checked={checked}
+          onChange={onChangeChecked}
+          className="ml-4 checkbox checkbox-primary input-sm"
+        />
+      </div>
     </section>
   )
 }
