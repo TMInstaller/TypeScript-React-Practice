7a1eeafc6569813811789217cca58771336a30a8
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-21 20:53:53 +0900

    feat : 시계 앱 완성

--- ch04/ch04_1/src/App.tsx
+++ ch04/ch04_1/src/App.tsx
@@ -2,19 +2,20 @@
 // 260p useEffect 훅 사용 초기 모습
 // 261p 화면 갱신 버그가 있는 버전
 // 262p useRef 훅 사용(여전히 화면은 갱신하지 않음)
-import { useEffect, useRef } from 'react'
+// 263p 시계 앱 완성(useRef 대신 useState 훅 사용)
+import { useEffect, useState } from 'react'
 import Clock from './pages/Clock'
 
 export default function App() {
-  let today = useRef(new Date())
+  const [today, setToday] = useState(new Date())
+
   useEffect(() => {
     console.log('useEffect called.')
     const duration = 1000
     const id = setInterval(() => {
-      today.current = new Date()
-      console.log('today', today.current.toLocaleTimeString())
+      setToday(new Date())
     }, duration)
     return () => clearInterval(id)
   }, [])
-  return <Clock today={today.current} />
+  return <Clock today={today} />
 }
