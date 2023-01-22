eabae4a56e2ecf0f6bea36fa8b2755a1a7ebe41d
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-23 02:31:23 +0900

    feat : 라디오 선택값 출력하기 구현

--- ch04/ch04_3/src/pages/RadioInputTest.tsx
+++ ch04/ch04_3/src/pages/RadioInputTest.tsx
@@ -1,10 +1,16 @@
 // 295p 라디오 버튼 구현하기(초기 모습)
-import { useMemo } from 'react'
+// 297p value 속성으로 라디오 버튼 선택 로직 구현하기
+import type { ChangeEvent } from 'react'
+import { useMemo, useCallback, useState } from 'react'
 import { Title, Subtitle } from '../components'
 import * as D from '../data'
 
 export default function RadioInputTest() {
   const jobTitles = useMemo(() => D.makeArray(4).map(D.randomJobTitle), [])
+  const [selectedJobTitle, setSelectedJobTitle] = useState<string>(jobTitles[0])
+  const onChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setSelectedJobTitle((notUsed) => e.target.value)
+  }, [])
   const radioInputs = useMemo(
     () =>
       jobTitles.map((value, index) => (
@@ -13,18 +19,21 @@ export default function RadioInputTest() {
             type="radio"
             name="jobs"
             className="mr-4 radio radio-primary"
+            checked={value === selectedJobTitle}
             defaultValue={value}
+            onChange={onChange}
           />
+          <span className="label-text">{value}</span>
         </label>
       )),
-    [jobTitles]
+    [jobTitles, selectedJobTitle, onChange]
   )
   return (
     <section className="mt-4">
       <Title>RadioInputTest</Title>
       <div className="flex flex-col justify-center mt-4">
         <Subtitle>What is your job?</Subtitle>
-        <Subtitle className="mt-4">Selected Job: </Subtitle>
+        <Subtitle className="mt-4">Selected Job: {selectedJobTitle}</Subtitle>
         <div className="flex justify-center p-4 mt-4 ">
           <div className="flex flex-col mt-4">{radioInputs}</div>
         </div>
