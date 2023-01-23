f20456652e49637c7ce77f4a29cb0d462b033058
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-23 18:50:36 +0900

    feat : forwardRefTest

--- ch04/ch04_5/src/pages/ForwardRefTest.tsx
+++ ch04/ch04_5/src/pages/ForwardRefTest.tsx
@@ -1,9 +1,32 @@
-// 230p 상대 경로 문제 해결하기
-export default function CopyMe() {
+// 352p Input 컴포넌트로 수정
+import { useRef, useCallback, useEffect } from 'react'
+import { Title } from '../components'
+import { Input } from '../theme/daisyui'
+
+export default function InputFocusTest() {
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const getValue = useCallback(
+    () => alert(`input value: ${inputRef.current?.value}`),
+    []
+  )
+  useEffect(() => inputRef.current?.focus(), [])
+
   return (
     <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center ">CopyMe</h2>
-      <div className="mt-4"></div>
+      <Title>ForwardRefTest</Title>
+      <div className="flex justify-center mt-4 ">
+        <div className="flex flex-col w-1/3 p-2 ">
+          <Input
+            ref={inputRef}
+            className="input input-primary"
+            placeholder="enter some text"
+          />
+          <button onClick={getValue} className="mt-4 btn btn-primary">
+            get value
+          </button>
+        </div>
+      </div>
     </section>
   )
 }
