9d3124c2162bff82d3135b1921aa56dc66cab1cc
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-23 18:42:01 +0900

    feat : inputFocusTest

--- ch04/ch04_5/src/pages/FileDrop.tsx
+++ ch04/ch04_5/src/pages/FileDrop.tsx
@@ -1,9 +1,99 @@
-// 230p 상대 경로 문제 해결하기
-export default function CopyMe() {
+// 345p 사용자의 이미지 파일을 웹 페이지에 표시하기
+import type { ChangeEvent, DragEvent } from 'react'
+import { useState, useRef, useCallback, useMemo } from 'react'
+import { useToggle } from '../hooks'
+import { Title, Div } from '../components'
+import { imageFileReaderP } from '../utils'
+
+export default function FileDrop() {
+  const [imageUrls, setImageUrls] = useState<string[]>([])
+  const [error, setError] = useState<Error | null>(null)
+  const [loading, toggleLoading] = useToggle(false)
+
+  const inputRef = useRef<HTMLInputElement>(null)
+  const onDivClick = useCallback(() => inputRef.current?.click(), [])
+
+  const makeImageUrls = useCallback(
+    (files: File[]) => {
+      const promises = Array.from(files).map(imageFileReaderP)
+      toggleLoading()
+      Promise.all(promises)
+        .then((urls) => setImageUrls((imageUrls) => [...urls, ...imageUrls]))
+        .catch(setError)
+        .finally(toggleLoading)
+    },
+    [toggleLoading]
+  )
+
+  const onInputChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      setError(null)
+      const files = e.target.files
+      files && makeImageUrls(Array.from(files))
+    },
+    [makeImageUrls]
+  )
+
+  const onDivDragOver = useCallback((e: DragEvent) => e.preventDefault(), [])
+  const onDivDrop = useCallback(
+    (e: DragEvent) => {
+      e.preventDefault()
+      setError(null)
+      const files = e.dataTransfer?.files
+      files && makeImageUrls(Array.from(files))
+    },
+    [makeImageUrls]
+  )
+
+  // prettier-ignore
+  const images = useMemo(
+    () =>
+      imageUrls.map((url, index) => (
+        <Div
+          key={index} src={url} width="5rem" height="5rem"
+          className="m-2 bg-transparent bg-center bg-no-repeat bg-contain "
+        />
+      )),
+    [imageUrls]
+  )
+
+  // prettier-ignore
   return (
     <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center ">CopyMe</h2>
-      <div className="mt-4"></div>
+      <Title>FileDrop</Title>
+      {error && (
+        <div className="p-4 mt-4 bg-red-200">
+          <p className="text-3xl text-red-500 text-bold">{error.message}</p>
+        </div>
+      )}
+
+      <div
+        onClick={onDivClick}
+        className="w-full mt-4 bg-gray-200 border border-gray-500 "
+      >
+        {loading && (
+          <div className="flex items-center justify-center ">
+            <button className=" btn btn-circle loading"></button>
+          </div>
+        )}
+
+        <div
+          onDragOver={onDivDragOver}
+          onDrop={onDivDrop}
+          className="flex flex-col items-center justify-center h-40 cursor-pointer "
+        >
+          <p className="text-3xl font-bold ">drop images or click</p>
+        </div>
+        <input
+          type="file" ref={inputRef}
+          onChange={onInputChange}
+          className="hidden"
+          accept="image/*"
+          multiple
+        />
+      </div>
+
+      <div className="flex flex-wrap justify-center ">{images}</div>
     </section>
   )
 }
--- ch04/ch04_5/src/pages/InputFocusTest.tsx
+++ ch04/ch04_5/src/pages/InputFocusTest.tsx
@@ -1,9 +1,19 @@
-// 230p 상대 경로 문제 해결하기
-export default function CopyMe() {
+// 348p 입력 상자에 자동 포커싱 구현하기
+import { useRef, useEffect } from 'react'
+import { Title } from '../components'
+
+export default function InputFocusTest() {
+  const inputRef = useRef<HTMLInputElement>(null)
   return (
     <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center ">CopyMe</h2>
-      <div className="mt-4"></div>
+      <Title>InputFocusTest</Title>
+      <div className="flex justify-center mt-4 ">
+        <input
+          ref={inputRef}
+          className="input input-primary"
+          placeholder="enter some text"
+        />
+      </div>
     </section>
   )
 }
