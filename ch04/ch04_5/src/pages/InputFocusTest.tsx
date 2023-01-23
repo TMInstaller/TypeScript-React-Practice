aab871642ce8656ca282c1c95139f08e5c05bdc0
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-23 18:42:52 +0900

    feat : inputFocusTest

--- ch04/ch04_5/src/pages/InputFocusTest.tsx
+++ ch04/ch04_5/src/pages/InputFocusTest.tsx
@@ -4,6 +4,8 @@ import { Title } from '../components'
 
 export default function InputFocusTest() {
   const inputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => inputRef.current?.focus(), [])
   return (
     <section className="mt-4">
       <Title>InputFocusTest</Title>
