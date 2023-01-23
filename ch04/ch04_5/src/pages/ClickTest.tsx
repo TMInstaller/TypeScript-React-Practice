db25ff9da795e607019da965fc6f8c4e9b0694e5
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-23 12:39:36 +0900

    feat : 버튼 클릭해서 이미지 파일 집어넣기

--- ch04/ch04_5/src/pages/ClickTest.tsx
+++ ch04/ch04_5/src/pages/ClickTest.tsx
@@ -1,9 +1,20 @@
-// 230p 상대 경로 문제 해결하기
-export default function CopyMe() {
+// 339p 이미지 파일을 대상으로 [열기] 대화 상자 띄우기
+import { useRef, useCallback } from 'react'
+import { Title } from '../components'
+
+export default function ClickTest() {
+  const inputRef = useRef<HTMLInputElement>(null)
+  const onClick = useCallback(() => inputRef.current?.click(), [])
+
   return (
     <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center ">CopyMe</h2>
-      <div className="mt-4"></div>
+      <Title>ClickTest</Title>
+      <div className="flex items-center justify-center mt-4 ">
+        <button onClick={onClick} className="mr-4 btn bg-primary">
+          Click Me
+        </button>
+        <input type="file" ref={inputRef} className="hidden" accept="image/*" />
+      </div>
     </section>
   )
 }
