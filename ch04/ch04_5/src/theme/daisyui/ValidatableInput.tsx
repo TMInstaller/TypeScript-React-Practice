0738dd15c817d6a5e57f64504dbff00c6a8f08bd
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-24 13:00:04 +0900

    feat : validateInput

--- ch04/ch04_5/src/theme/daisyui/ValidatableInput.tsx
+++ ch04/ch04_5/src/theme/daisyui/ValidatableInput.tsx
@@ -5,3 +5,34 @@ import { forwardRef, useImperativeHandle, useMemo, useRef } from 'react'
 export type ValidatableInputMethods = {
   validate: () => [boolean, string]
 }
+
+export const ValidatableInput = forwardRef<
+  ValidatableInputMethods,
+  ReactInputProps
+>(({ type, className: _className, ...inputProps }, methodRef) => {
+  const className = useMemo(() => ['input', _className].join(' '), [_className])
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  useImperativeHandle(
+    methodRef,
+    () => ({
+      validate: (): [boolean, string] => {
+        const value = inputRef.current?.value
+        if (!value || !value.length)
+          return [false, '사용자가 입ㅕ한 내용이 없습니다']
+
+        switch (type) {
+          case 'email': {
+            const regEx =
+              /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+            const valid = regEx.test(value)
+            return valid ? [true, value] : [false, '틀린 이메일 주소입니다']
+          }
+        }
+        return [true, value]
+      },
+    }),
+    [type]
+  )
+  return <input {...inputProps} className={className} ref={inputRef} />
+})
