c79a617f11824efac2a77aeaf3e24c5b17fe7765
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-24 21:15:53 +0900

    feat : useReducer 시계

--- ch05/ch05_1/src/pages/UseReducerClock.tsx
+++ ch05/ch05_1/src/pages/UseReducerClock.tsx
@@ -1,9 +1,36 @@
-// 230p 상대 경로 문제 해결하기
-export default function CopyMe() {
+// 382p 리덕스 시계 구현하기
+// 387p 시계 완성하기
+// 389p useReducer 훅으로 시계 완성하기
+import { useReducer } from 'react'
+import type { AppState } from '../store'
+import { Div, Title, Subtitle } from '../components'
+import { useInterval } from '../hooks'
+import { SetTodayAction } from '../store/actions'
+
+export default function ReduxClock() {
+  const [{ today }, dispatch] = useReducer(
+    (state: AppState, action: SetTodayAction) => {
+      switch (action.type) {
+        case 'setToday':
+          return { ...state, today: new Date() }
+      }
+    },
+    {
+      today: new Date(),
+    }
+  )
+
+  useInterval(() => {
+    dispatch({ type: 'setToday', today: new Date() })
+  })
+
   return (
-    <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center ">CopyMe</h2>
-      <div className="mt-4"></div>
-    </section>
+    <Div className="flex flex-col items-center justify-center h-24 ">
+      <Title className="text-5xl ">UseReduxClock</Title>
+      <Title className="mt-4 text-3xl ">{today.toLocaleTimeString()}</Title>
+      <Subtitle className="mt-4 text-2xl ">
+        {today.toLocaleDateString()}
+      </Subtitle>
+    </Div>
   )
 }
