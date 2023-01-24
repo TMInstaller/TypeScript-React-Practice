ebe8118f815f91139c205ab25963ce93209badc0
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-24 13:15:56 +0900

    chore : ch04_6 setting

---
+++ ch04/ch04_6/src/App.tsx
@@ -0,0 +1,10 @@
+// 363p 기본 앱 파일 작성하기
+import ResponsiveContextTest from './pages/ResponsiveContextTest'
+
+export default function App() {
+  return (
+    <main>
+      <ResponsiveContextTest />
+    </main>
+  )
+}
---
+++ ch04/ch04_6/src/hooks/useEventListener.ts
@@ -0,0 +1,15 @@
+// 323p 이벤트 처리기 커스텀 훅 만들기
+import { useEffect } from 'react'
+
+export const useEventListener = (
+  target: EventTarget | null,
+  type: string,
+  callback: EventListenerOrEventListenerObject | null
+) => {
+  useEffect(() => {
+    if (target && callback) {
+      target.addEventListener(type, callback)
+      return () => target.removeEventListener(type, callback)
+    }
+  }, [target, type, callback])
+}
