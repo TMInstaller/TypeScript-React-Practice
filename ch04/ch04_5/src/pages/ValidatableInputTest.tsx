24fb1a48048b75b13a365a3b026be1ca3418a002
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-24 12:59:02 +0900

    feat : validateInput

--- ch04/ch04_5/src/pages/ValidatableInputTest.tsx
+++ ch04/ch04_5/src/pages/ValidatableInputTest.tsx
@@ -1,39 +1,17 @@
-// 359p ValidatableInput 컴포넌트 완성하기
-import type { ReactInputProps } from '../theme/daisyui'
-import { forwardRef, useImperativeHandle, useMemo, useRef } from 'react'
+// 360p 이메일 형식 검증하기
+import { useRef, useCallback } from 'react'
+import { Title } from '../components'
+import type { ValidatableInputMethods } from '../theme/daisyui'
+import { ValidatableInput } from '../theme/daisyui'
 
-export type ValidatableInputMethods = {
-  validate: () => [boolean, string]
+export default function ValidatableInputTest() {
+  const methodsRef = useRef<ValidatableInputMethods>(null)
+
+  const validateEmail = useCallback(() => {
+    if (methodsRef.current) {
+      const [valid, valueOrErrorMessage] = methodsRef.current.validate()
+      if (valid) alert(`${valueOrErrorMessage}는 유효한 이메일 주소입니다`)
+      else alert(valueOrErrorMessage)
+    }
+  }, [])
 }
-
-export const ValidatableInput = forwardRef<
-  ValidatableInputMethods,
-  ReactInputProps
->(({ type, className: _className, ...inputProps }, methodsRef) => {
-  const className = useMemo(() => ['input', _className].join(' '), [_className])
-  const inputRef = useRef<HTMLInputElement>(null)
-
-  useImperativeHandle(
-    methodsRef,
-    () => ({
-      validate: (): [boolean, string] => {
-        const value = inputRef.current?.value
-        if (!value || !value.length)
-          return [false, '사용자가 입력한 내용이 없습니다']
-
-        switch (type) {
-          case 'email': {
-            const regEx =
-              /[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,8}(.[a-z{2,8}])?/g
-            const valid = regEx.test(value)
-            return valid ? [true, value] : [false, '틀린 이메일 주소입니다']
-          }
-        }
-        return [true, value]
-      },
-    }),
-    [type]
-  )
-
-  return <input {...inputProps} className={className} ref={inputRef} />
-})
