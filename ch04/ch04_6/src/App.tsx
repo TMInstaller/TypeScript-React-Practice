681baed12886809479f193ae1e750b033c6b71c0
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-24 13:26:34 +0900

    feat : 앱에 ResponsiveProvider 적용

--- ch04/ch04_6/src/App.tsx
+++ ch04/ch04_6/src/App.tsx
@@ -1,10 +1,14 @@
 // 363p 기본 앱 파일 작성하기
+// 369p 앱 파일 수정하기
 import ResponsiveContextTest from './pages/ResponsiveContextTest'
+import { ResponsiveProvider } from './contexts'
 
 export default function App() {
   return (
-    <main>
-      <ResponsiveContextTest />
-    </main>
+    <ResponsiveProvider>
+      <main>
+        <ResponsiveContextTest />
+      </main>
+    </ResponsiveProvider>
   )
 }
