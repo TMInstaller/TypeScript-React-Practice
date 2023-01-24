0a7ad31e6dd4fba2bd567234864a6866658a6b01
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-24 13:50:43 +0900

    feat: 리덕스 시계 초기 모습

--- ch05/ch05_1/src/App.tsx
+++ ch05/ch05_1/src/App.tsx
@@ -1,12 +1,29 @@
 // 374p 기본 앱 파일 작성하기
+// 378p 리덕스로 시계 구현하기(초기 모습)
+import type { Action } from 'redux'
+import { Provider as ReduxProvider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+
 import ReduxClock from './pages/ReduxClock'
 import UseReducerClock from './pages/UseReducerClock'
 
+type AppState = {
+  today: Date
+}
+
+const initialAppState = {
+  today: new Date(),
+}
+const rootReducer = (state: AppState = initialAppState, action: Action) => state
+const store = configureStore({ reducer: rootReducer, middleware: [] })
+
 export default function App() {
   return (
-    <main className="p-8 ">
-      <UseReducerClock />
-      <ReduxClock />
-    </main>
+    <ReduxProvider store={store}>
+      <main className="p-8 ">
+        <UseReducerClock />
+        <ReduxClock />
+      </main>
+    </ReduxProvider>
   )
 }
