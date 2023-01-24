572476fe3fe98a48d2aa07ea1aabebd9f0e79a8b
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-24 21:04:42 +0900

    feat : roodReducer

--- ch05/ch05_1/src/store/rootReducer.ts
+++ ch05/ch05_1/src/store/rootReducer.ts
@@ -1,5 +1,7 @@
 // 379p 루트 리듀서 코드 옮기기
-import type { Action } from 'redux'
+// 384p 리듀서에 액션 추가하기
+// 385p 리듀서에 SetTodayAction 반영하기
+import type { Actions } from './actions'
 import type { AppState } from './AppState'
 
 const initialAppState = {
@@ -7,5 +9,12 @@ const initialAppState = {
 }
 export const rootReducer = (
   state: AppState = initialAppState,
-  action: Action
-) => state
+  action: Actions
+) => {
+  switch (action.type) {
+    case 'setToday': {
+      return { ...state, today: action.today }
+    }
+  }
+  return state // 필수
+}
