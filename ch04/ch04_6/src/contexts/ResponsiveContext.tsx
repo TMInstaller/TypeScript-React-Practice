62d562e92a421bf931f321a52e348fcf0b655490
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-24 13:25:31 +0900

    feat : ResponsiveContext

--- ch04/ch04_6/src/contexts/ResponsiveContext.tsx
+++ ch04/ch04_6/src/contexts/ResponsiveContext.tsx
@@ -1,4 +1,6 @@
 // 366p 반응형 컨텍스트 구현하기(초기 모습)
+// 368p ResponsiveProvider 구현하기
+import type { FC, PropsWithChildren } from 'react'
 import { createContext } from 'react'
 
 type ContextType = {
@@ -8,3 +10,14 @@ const defaultContextValue: ContextType = {
   breakpoint: '', // 공유할 데이터 속성 초기값
 }
 export const ResponsiveContext = createContext<ContextType>(defaultContextValue)
+
+type ResponsiveProviderProps = {}
+export const ResponsiveProvider: FC<
+  PropsWithChildren<ResponsiveProviderProps>
+> = ({ children, ...props }) => {
+  const breakpoint = 'sm'
+  const value = {
+    breakpoint, // breakpoint: breakpoint 코드를 간결하게 표현
+  }
+  return <ResponsiveContext.Provider value={value} children={children} />
+}
