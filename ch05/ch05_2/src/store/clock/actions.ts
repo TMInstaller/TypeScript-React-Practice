04ea9c63da3c2ddfefe528dd211a6ff94caa7d1a
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-25 13:50:33 +0900

    feat : clock action

--- ch05/ch05_2/src/store/clock/actions.ts
+++ ch05/ch05_2/src/store/clock/actions.ts
@@ -1,2 +1,7 @@
-// 392p 액션 파일 작성하기
+// 399p 액션 생성기 작성하기
 import type * as T from './types'
+
+export const setClock = (payload: T.State): T.SetClockAction => ({
+  type: '@clock/setClock',
+  payload,
+})
