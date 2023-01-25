221cbedb8b9a05545d207de7738e60481fa86d7a
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-25 13:52:00 +0900

    feat : clock reducers

--- ch05/ch05_2/src/store/clock/reducers.ts
+++ ch05/ch05_2/src/store/clock/reducers.ts
@@ -1,8 +1,12 @@
-// 392p 리듀서 파일 작성하기
+// 399p 리듀서 작성하기
 import * as T from './types'
 
-const initialState: T.State = {}
+const initialState: T.State = new Date()
 
 export const reducer = (state: T.State = initialState, action: T.Actions) => {
+  switch (action.type) {
+    case '@clock/setClock':
+      return action.payload
+  }
   return state
 }
