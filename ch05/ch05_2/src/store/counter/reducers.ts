80352238881b280094746bc00fa370c6fd58f088
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-26 00:17:10 +0900

    feat : counter reducers

--- ch05/ch05_2/src/store/counter/reducers.ts
+++ ch05/ch05_2/src/store/counter/reducers.ts
@@ -1,8 +1,12 @@
-// 392p 리듀서 파일 작성하기
+// 403p 리듀서 작성하기
 import * as T from './types'
 
-const initialState: T.State = {}
+const initialState: T.State = 0
 
 export const reducer = (state: T.State = initialState, action: T.Actions) => {
+  switch (action.type) {
+    case '@counter/setCounter':
+      return state + action.payload
+  }
   return state
 }
