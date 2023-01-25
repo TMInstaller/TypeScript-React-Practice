1febfe113683e8749473ec5c9f07530df5301a21
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-26 00:15:27 +0900

    feat : counter actions

--- ch05/ch05_2/src/store/counter/actions.ts
+++ ch05/ch05_2/src/store/counter/actions.ts
@@ -1,2 +1,9 @@
-// 392p 액션 파일 작성하기
+// 402p 액션 생성기 작성하기
 import type * as T from './types'
+
+export const setCounter = (payload: T.State): T.SetCounterAction => ({
+  type: '@counter/setCounter',
+  payload,
+})
+export const increaseCounter = () => setCounter(1)
+export const decreaseCounter = () => setCounter(-1)
