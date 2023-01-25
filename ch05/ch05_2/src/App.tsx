d04f65355dc8a7e982997fc51da76635ca385a75
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-25 13:47:51 +0900

    feat : setting

--- ch05/ch05_2/src/App.tsx
+++ ch05/ch05_2/src/App.tsx
@@ -1,13 +1,21 @@
 // 394p 기본 앱 파일
+// 397p 앱 파일에 컴포넌트 추가하기
 import { Provider as ReduxProvider } from 'react-redux'
 import { useStore } from './store'
+import CardsTest from './pages/CardsTest'
+import ClockTest from './pages/ClockTest'
+import CounterTest from './pages/CounterTest'
+import RemoteUserTest from './pages/RemoteUserTest'
 
 export default function App() {
   const store = useStore()
   return (
     <ReduxProvider store={store}>
       <main className="p-8 ">
-        <div />
+        <CardsTest />
+        <RemoteUserTest />
+        <CounterTest />
+        <ClockTest />
       </main>
     </ReduxProvider>
   )
