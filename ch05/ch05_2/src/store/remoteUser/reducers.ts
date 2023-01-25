94005dcd1f5b7a9b51d8ce8ecbd388237847a183
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-26 00:50:33 +0900

    feat : remoteUser reducers

--- ch05/ch05_2/src/store/remoteUser/reducers.ts
+++ ch05/ch05_2/src/store/remoteUser/reducers.ts
@@ -1,8 +1,22 @@
-// 392p 리듀서 파일 작성하기
+// 411p 리듀서 작성하기
 import * as T from './types'
 
-const initialState: T.State = {}
+const initialState: T.State = {
+  email: '',
+  name: { title: '', first: '', last: '' },
+  picture: { large: '' },
+}
 
 export const reducer = (state: T.State = initialState, action: T.Actions) => {
+  switch (action.type) {
+    case '@remoteUser/setUser':
+      return action.payload
+    case '@remoteUser/changeEmail':
+      return { ...state, email: action.payload }
+    case '@remoteUser/changeName':
+      return { ...state, name: action.payload }
+    case '@remoteUser/changePicture':
+      return { ...state, picture: action.payload }
+  }
   return state
 }
