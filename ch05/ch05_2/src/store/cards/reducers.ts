1b60f2c2b69b40673c54f5d780d3150a2bfb277a
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-26 13:23:22 +0900

    feat : card reducer

--- ch05/ch05_2/src/store/cards/reducers.ts
+++ ch05/ch05_2/src/store/cards/reducers.ts
@@ -1,8 +1,14 @@
-// 392p 리듀서 파일 작성하기
+// 416p 리듀서 작성하기
 import * as T from './types'
 
-const initialState: T.State = {}
+const initialState: T.State = []
 
 export const reducer = (state: T.State = initialState, action: T.Actions) => {
+  switch (action.type) {
+    case '@cards/addCard':
+      return [action.payload, ...state]
+    case '@cards/removeCard':
+      return state.filter((card) => card.uuid != action.payload)
+  }
   return state
 }
