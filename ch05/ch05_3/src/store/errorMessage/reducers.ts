6f771a1c6af2ac9e8f266cf08122088c00f56287
TMInstaller <ajvldmlqjqclr@naver.com>
2023-02-05 15:22:05 +0900

    feat : 리듀서 작성하기

--- ch05/ch05_3/src/store/errorMessage/reducers.ts
+++ ch05/ch05_3/src/store/errorMessage/reducers.ts
@@ -1,8 +1,12 @@
-// 392p 리듀서 파일 작성하기
+// 434p 리듀서 작성하기
 import * as T from './types'
 
-const initialState: T.State = {}
+const initialState: T.State = ''
 
 export const reducer = (state: T.State = initialState, action: T.Actions) => {
+  switch (action.type) {
+    case '@errorMessage/setErrorMessage':
+      return action.payload
+  }
   return state
 }
