126583473b22d29bb3929b77d5eebd394978249d
TMInstaller <ajvldmlqjqclr@naver.com>
2023-02-05 15:03:57 +0900

    feat : 액션 생성기 작성하기

--- ch05/ch05_3/src/store/loading/actions.ts
+++ ch05/ch05_3/src/store/loading/actions.ts
@@ -1,2 +1,7 @@
-// 392p 액션 파일 작성하기
+// 429p 액션 생성기 작성하기
 import type * as T from './types'
+
+export const setLoading = (payload: T.State): T.SetLoadingAction => ({
+  type: '@loading/setLoadingAction',
+  payload,
+})
