3ad4ab6a3350fa091a371113472d0afc45063041
TMInstaller <ajvldmlqjqclr@naver.com>
2023-02-05 15:05:25 +0900

    feat : 리듀서 작성하기

--- ch05/ch05_3/src/store/loading/reducers.ts
+++ ch05/ch05_3/src/store/loading/reducers.ts
@@ -1,8 +1,12 @@
-// 392p 리듀서 파일 작성하기
+// 429p 리듀서 작성하기
 import * as T from './types'
 
-const initialState: T.State = {}
+const initialState: T.State = false
 
 export const reducer = (state: T.State = initialState, action: T.Actions) => {
+  switch (action.type) {
+    case '@loading/setLoadingAction':
+      return action.payload
+  }
   return state
 }
