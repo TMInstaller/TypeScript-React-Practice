eac97b0620823f735a59c9cee283c9d5c11025de
TMInstaller <ajvldmlqjqclr@naver.com>
2023-02-05 12:06:37 +0900

    feat : 앱 상태 작성하기

--- ch05/ch05_3/src/store/AppState.ts
+++ ch05/ch05_3/src/store/AppState.ts
@@ -1,2 +1,10 @@
-// 390P 앱 상태 파일 작성하기
-export type AppState = {}
+// 419P 앱 상태 파일 작성하기
+import * as L from './loading'
+import * as E from './errorMessage'
+import * as F from './fetchUser'
+
+export type AppState = {
+  loading: L.State
+  errorMessage: E.State
+  fetchUser: F.State
+}
