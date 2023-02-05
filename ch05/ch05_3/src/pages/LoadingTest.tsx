75ac61c737e7385ab636bedb1bf9bc1c4093dec0
TMInstaller <ajvldmlqjqclr@naver.com>
2023-02-05 15:16:36 +0900

    feat : LoadingTest

--- ch05/ch05_3/src/pages/LoadingTest.tsx
+++ ch05/ch05_3/src/pages/LoadingTest.tsx
@@ -1,9 +1,38 @@
-// 230p 상대 경로 문제 해결하기
-export default function CopyMe() {
+// 로딩 썽크 액션 테스트하기
+import { useCallback } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import type { AppState } from '../store'
+import { Title } from '../components'
+import { Button } from '../theme/daisyui'
+import * as L from '../store/loading'
+
+export default function LoadingTest() {
+  const dispatch = useDispatch()
+  const loading = useSelector<AppState, L.State>(({ loading }) => loading)
+
+  const doTimedLoading = useCallback(() => {
+    dispatch<any>(L.doTimedLoading(1000))
+  }, [dispatch])
+
   return (
     <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center ">CopyMe</h2>
-      <div className="mt-4"></div>
+      <Title>LoadingTest</Title>
+      <div className="mt-4">
+        <div className="flex justify-center mt-4 ">
+          <Button
+            className=" btn-sm btn-primary"
+            onClick={doTimedLoading}
+            disabled={loading}
+          >
+            do timed loading
+          </Button>
+        </div>
+        {loading && (
+          <div className="flex items-center justify-center ">
+            <button className=" btn btn-circle loading"></button>
+          </div>
+        )}
+      </div>
     </section>
   )
 }
