8f41aa8cfb8cd5250be0975b02c1919c99fcac71
TMInstaller <ajvldmlqjqclr@naver.com>
2023-02-05 14:55:22 +0900

    feat : 썽크 미들웨어 적용하기

--- ch05/ch05_3/src/store/useStore.ts
+++ ch05/ch05_3/src/store/useStore.ts
@@ -4,11 +4,13 @@ import { useMemo } from 'react'
 import { rootReducer } from './rootReducer'
 // 426p redux-logger 패키지로 수정하기
 import logger from 'redux-logger'
+// 428p 썽크 미들웨어 적용하기
+import thunk from 'redux-thunk'
 
 const useLogger = process.env.NODE_ENV !== 'production'
 
 const initializaStore = () => {
-  const middleware: any[] = []
+  const middleware: any[] = [thunk]
   if (useLogger) {
     middleware.push(logger)
   }
