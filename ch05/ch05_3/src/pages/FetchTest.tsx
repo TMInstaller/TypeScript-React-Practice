c4655b24d9dfb30f8fe307c51205ce420016e7f4
TMInstaller <ajvldmlqjqclr@naver.com>
2023-02-05 15:49:09 +0900

    feat : 썽크 액션

--- ch05/ch05_3/src/pages/FetchTest.tsx
+++ ch05/ch05_3/src/pages/FetchTest.tsx
@@ -1,9 +1,77 @@
-// 230p 상대 경로 문제 해결하기
-export default function CopyMe() {
+// 439p 썽크 액션으로 사용자 정보 변경 화면 작성하기
+import { useCallback, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { Title, Avatar } from '../components'
+import { Button } from '../theme/daisyui'
+import * as D from '../data'
+import type { AppState } from '../store'
+import * as F from '../store/fetchUser'
+
+export default function FetchTest() {
+  const dispatch = useDispatch()
+  const {
+    loading,
+    errorMessage,
+    fetchUser: user,
+  } = useSelector<AppState, AppState>((state) => state)
+
+  const getRemoteUser = useCallback(() => {
+    dispatch<any>(F.getRemoteUser())
+  }, [dispatch])
+  const changeName = useCallback(() => {
+    dispatch<any>(F.getRemoteUser())
+  }, [dispatch])
+  const changeEmail = useCallback(
+    () => dispatch(F.changeEmail(D.randomEmail())),
+    [dispatch]
+  )
+  const changePicture = useCallback(
+    () => dispatch(F.changePicture({ large: D.randomAvatar() })),
+    [dispatch]
+  )
+
+  useEffect(getRemoteUser, [getRemoteUser])
+
   return (
     <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center ">CopyMe</h2>
-      <div className="mt-4"></div>
+      <Title>FetchTest</Title>
+      <div className="flex justify-center mt-4">
+        <Button className="btn-sm btn bg-primary" onClick={getRemoteUser}>
+          get remote user
+        </Button>
+        <Button className="ml-4  btn-sm btn bg-accent" onClick={changeName}>
+          change name
+        </Button>
+        <Button className="ml-4  btn-sm btn bg-success" onClick={changeEmail}>
+          change email
+        </Button>
+        <Button
+          className="ml-4  btn-sm btn bg-secondary"
+          onClick={changePicture}
+        >
+          change picture
+        </Button>
+      </div>
+      {loading && (
+        <div className="flex items-center justify-center ">
+          <button className=" btn btn-circle loading"></button>
+        </div>
+      )}
+      {errorMessage.length && (
+        <div className="p-4 mt-4 bg-red-200 ">
+          <p className="text-3xl text-red-500 text-bold">{errorMessage}</p>
+        </div>
+      )}
+
+      <div className="flex justify-center p-4 m-4 ">
+        <Avatar src={user.picture.large} />
+        <div className="ml-4 ">
+          <p className="text-xl text-bold">
+            {user.name.title}. {user.name.first} {user.name.last}
+          </p>
+          <p className="italic to-gray-600">{user.email}</p>
+        </div>
+      </div>
     </section>
   )
 }
