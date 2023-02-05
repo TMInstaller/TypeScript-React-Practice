7ed7ebf5d8a887750d85af94a21aa59aa4c10afe
TMInstaller <ajvldmlqjqclr@naver.com>
2023-02-05 14:47:37 +0900

    feat : 개발 모드일 때에만 기록하도록 설정하기

--- ch05/ch05_3/src/store/useStore.ts
+++ ch05/ch05_3/src/store/useStore.ts
@@ -1,9 +1,17 @@
-// 391p 스토어 파일 작성하기
+// 424p 개발 모드일 때에만 기록하도록 설정하기
 import { configureStore } from '@reduxjs/toolkit'
 import { useMemo } from 'react'
 import { rootReducer } from './rootReducer'
+import logger from './logger'
+
+const useLogger = process.env.NODE_ENV !== 'production'
 
 const initializaStore = () => {
+  const middleware: any[] = []
+  if (useLogger) {
+    middleware.push(logger)
+  }
+
   const store = configureStore({ reducer: rootReducer, middleware: [] })
   return store
 }
