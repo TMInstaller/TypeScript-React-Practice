623f3112e1d9cfec1381605ab63b0a9e10b44272
TMInstaller <ajvldmlqjqclr@naver.com>
2023-02-08 11:52:44 +0900

    feat : 타입 선언하기

--- ch05/ch05_4/src/store/listOrders/types.ts
+++ ch05/ch05_4/src/store/listOrders/types.ts
@@ -1,5 +1,15 @@
-// 392p 타입 파일 작성하기
+// 452p 타입 선언하기
 import type { Action } from 'redux'
+import type { UUID } from '../commonTypes'
+export * from '../commonTypes'
 
-export type State = any
-export type Actions = Action
+export type State = UUID[]
+export type SetListidOrders = Action<'@listidOrders/set'> & {
+  payload: State
+}
+export type AddListidOrders = Action<'@listidOrders/add'> & { payload: UUID }
+export type RemoveListidFromOrders = Action<'@listOrders/remove'> & {
+  payload: UUID
+}
+
+export type Actions = SetListidOrders | AddListidOrders | RemoveListidFromOrders
