1c03b7cd355ed3d2d679064521a059c2ea0ed5a2
TMInstaller <ajvldmlqjqclr@naver.com>
2023-02-08 11:24:56 +0900

    feat : 기본 앱 파일 작성하기

--- ch05/ch05_4/src/App.tsx
+++ ch05/ch05_4/src/App.tsx
@@ -1,19 +1,17 @@
-// 420p 기본 앱 파일
+// 446p 기본 앱 파일 작성하기
 import { Provider as ReduxProvider } from 'react-redux'
-import ErrorMessageTest from './pages/ErrorMessageTest'
-import FetchTest from './pages/FetchTest'
-import LoadingTest from './pages/LoadingTest'
-import LoggerTest from './pages/LoggerTest'
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 import { useStore } from './store'
+import Board from './pages/Board'
 
 export default function App() {
   const store = useStore()
   return (
     <ReduxProvider store={store}>
-      <FetchTest />
-      <ErrorMessageTest />
-      <LoadingTest />
-      <LoggerTest />
+      <DndProvider backend={HTML5Backend}>
+        <Board />
+      </DndProvider>
     </ReduxProvider>
   )
 }
---
+++ ch05/ch05_4/src/pages/Board/index.tsx
@@ -0,0 +1,9 @@
+// 230p 상대 경로 문제 해결하기
+export default function CopyMe() {
+  return (
+    <section className="mt-4">
+      <h2 className="text-5xl font-bold text-center ">CopyMe</h2>
+      <div className="mt-4"></div>
+    </section>
+  )
+}
