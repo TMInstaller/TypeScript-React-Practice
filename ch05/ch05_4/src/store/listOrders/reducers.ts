faa5de8eb87f52b55f926a70f4d5f5d8c24516e0
TMInstaller <ajvldmlqjqclr@naver.com>
2023-02-08 11:58:27 +0900

    feat : 리듀서 작성하기

--- ch05/ch05_4/src/store/listOrders/reducers.ts
+++ ch05/ch05_4/src/store/listOrders/reducers.ts
@@ -1,8 +1,16 @@
-// 392p 리듀서 파일 작성하기
+// 453p 리듀서 작성하기
 import * as T from './types'
 
-const initialState: T.State = {}
+const initialState: T.State = []
 
 export const reducer = (state: T.State = initialState, action: T.Actions) => {
+  switch (action.type) {
+    case '@listidOrders/set':
+      return action.payload
+    case '@listidOrders/add':
+      return [...state, action.payload]
+    case '@listOrders/remove':
+      return state.filter((uuid) => uuid !== action.payload)
+  }
   return state
 }
