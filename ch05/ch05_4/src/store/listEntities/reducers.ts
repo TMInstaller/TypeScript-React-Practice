65accfc10815f546be32bd8e667a3100d19ec42f
TMInstaller <ajvldmlqjqclr@naver.com>
2023-02-19 01:43:07 +0900

    456p

--- ch05/ch05_4/src/store/listEntities/reducers.ts
+++ ch05/ch05_4/src/store/listEntities/reducers.ts
@@ -1,8 +1,17 @@
-// 392p 리듀서 파일 작성하기
+// 456p 리듀서 작성하기
 import * as T from './types'
 
 const initialState: T.State = {}
 
 export const reducer = (state: T.State = initialState, action: T.Actions) => {
+  switch (action.type) {
+    case '@listEntities/add':
+      return { ...state, [action.payload.uuid]: action.payload }
+    case '@listEntities/remove': {
+      const newState = { ...state }
+      delete newState[action.payload]
+      return newState
+    }
+  }
   return state
 }
