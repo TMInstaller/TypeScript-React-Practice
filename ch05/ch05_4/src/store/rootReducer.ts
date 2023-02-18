206abe26c91a2ec38f057ee03397947e01a0baab
TMInstaller <ajvldmlqjqclr@naver.com>
2023-02-19 02:45:57 +0900

    446p

--- ch05/ch05_4/src/store/rootReducer.ts
+++ ch05/ch05_4/src/store/rootReducer.ts
@@ -1,7 +1,7 @@
 // 446p 루트 리듀서 작성하기
 import { combineReducers } from 'redux'
 import * as L from './listEntities'
-import * as LO from './listOrders'
+import * as LO from './listidOrders'
 import * as LC from './listidCardidOrders'
 import * as C from './cardEntities'
 
