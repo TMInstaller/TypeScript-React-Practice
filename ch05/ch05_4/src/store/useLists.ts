edfa30b79e9fac919ef34694bf99624e7e9a9d78
TMInstaller <ajvldmlqjqclr@naver.com>
2023-02-19 06:59:09 +0900

    470p

--- ch05/ch05_4/src/store/useLists.ts
+++ ch05/ch05_4/src/store/useLists.ts
@@ -1,10 +1,13 @@
 // 461p useLists 커스텀 훅 만들기(초기 모습)
+// 470p useLists 훅에 반영하기
 import { useCallback } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import type { AppState } from '../store'
 import type { List } from './commonTypes'
 import * as LO from './listidOrders'
 import * as L from '../store/listEntities'
+import * as C from '../store/cardEntities'
+import * as LC from '../store/listidCardidOrders'
 
 export const useLists = () => {
   const dispatch = useDispatch()
@@ -13,19 +16,29 @@ export const useLists = () => {
     ({ listidOrders, listEntities }) =>
       listidOrders.map((uuid) => listEntities[uuid])
   )
+  const listidCardidOrders = useSelector<AppState, LC.State>(
+    ({ listidCardidOrders }) => listidCardidOrders
+  )
   const onCreateList = useCallback(
-    (listid: string) => () => {
-      dispatch(L.removeList(listid))
-      dispatch(LO.removeListidFromOrders(listid))
+    (uuid: string, title: string) => () => {
+      const list = { uuid, title }
+      dispatch(LO.addListidToOrders(uuid))
+      dispatch(L.addList(list))
+      dispatch(LC.setListidCardIds({ listid: list.uuid, cardids: [] }))
     },
     [dispatch]
   )
   const onRemoveList = useCallback(
     (listid: string) => () => {
+      listidCardidOrders[listid].forEach((cardid) => {
+        dispatch(C.removeCard(cardid))
+      })
+      dispatch(LC.removeListid(listid))
+
       dispatch(L.removeList(listid))
       dispatch(LO.removeListidFromOrders(listid))
     },
-    [dispatch]
+    [dispatch, listidCardidOrders]
   )
   return { lists, onCreateList, onRemoveList }
 }
