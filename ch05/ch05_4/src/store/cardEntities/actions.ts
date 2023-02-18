41491f6f584bd16b77bbea0ef1a2e31969404c00
TMInstaller <ajvldmlqjqclr@naver.com>
2023-02-19 02:51:04 +0900

    465p

--- ch05/ch05_4/src/store/cardEntities/actions.ts
+++ ch05/ch05_4/src/store/cardEntities/actions.ts
@@ -1,2 +1,11 @@
-// 392p 액션 파일 작성하기
+// 465p 생성기 작성하기
 import type * as T from './types'
+
+export const addCard = (payload: T.Card): T.AddCardAction => ({
+  type: '@cardEntities/add',
+  payload,
+})
+export const removeCard = (payload: T.UUID): T.RemoveCardAction => ({
+  type: '@cardEntities/remove',
+  payload,
+})
