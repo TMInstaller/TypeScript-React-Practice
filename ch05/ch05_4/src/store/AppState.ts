2f724cf0f6313be7e6f233fa0f48208c427f1b6b
TMInstaller <ajvldmlqjqclr@naver.com>
2023-02-19 02:45:51 +0900

    445p

--- ch05/ch05_4/src/store/AppState.ts
+++ ch05/ch05_4/src/store/AppState.ts
@@ -1,12 +1,12 @@
 // 445p 앱 상태 파일 작성하기
 import * as L from './listEntities'
-import * as LO from './listOrders'
+import * as LO from './listidOrders'
 import * as LC from './listidCardidOrders'
 import * as C from './cardEntities'
 
 export type AppState = {
   listEntities: L.State
-  listOrders: LO.State
+  listidOrders: LO.State
   listidCardidOrders: LC.State
   cardEntities: C.State
 }
