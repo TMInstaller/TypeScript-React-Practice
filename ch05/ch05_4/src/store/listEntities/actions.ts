f609c5a7b0342dc92f4f40984763aa35dca85e75
TMInstaller <ajvldmlqjqclr@naver.com>
2023-02-19 01:40:36 +0900

    454p

--- ch05/ch05_4/src/store/listEntities/actions.ts
+++ ch05/ch05_4/src/store/listEntities/actions.ts
@@ -1,2 +1,11 @@
-// 392p 액션 파일 작성하기
+// 454p 액션 생성기 작성하기
 import type * as T from './types'
+
+export const addList = (payload: T.List): T.AddListAction => ({
+  type: '@listEntities/add',
+  payload,
+})
+export const removeList = (payload: string): T.RemoveListAction => ({
+  type: '@listEntities/remove',
+  payload,
+})
