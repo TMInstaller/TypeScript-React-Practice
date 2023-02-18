e407c2d2b1bf66915e9f8a700461ed1d880fa32c
TMInstaller <ajvldmlqjqclr@naver.com>
2023-02-19 06:51:01 +0900

    467p

--- ch05/ch05_4/src/store/listidCardidOrders/actions.ts
+++ ch05/ch05_4/src/store/listidCardidOrders/actions.ts
@@ -1,2 +1,32 @@
-// 392p 액션 파일 작성하기
+// 467p 액션 생성기 작성하기
 import type * as T from './types'
+
+export const setListidCardIds = (
+  payload: T.ListidCardidS
+): T.SetListidCardIds => ({
+  type: '@listidCardids/set',
+  payload,
+})
+export const removeListid = (payload: string): T.RemoveListidAction => ({
+  type: '@listidCardids/remove',
+  payload,
+})
+
+export const prependCardidToListidAction = (
+  payload: T.ListidCardid
+): T.PrependCardidToListidAction => ({
+  type: '@listidCardids/prependCardid',
+  payload,
+})
+export const appendCardidToListid = (
+  payload: T.ListidCardid
+): T.AppendCardidToListidAction => ({
+  type: '@listidCardids/appendCardid',
+  payload,
+})
+export const removeCardIdFromListId = (
+  payload: T.ListidCardid
+): T.RemoveCardidToListidAction => ({
+  type: '@listidCardids/removeCardid',
+  payload,
+})
