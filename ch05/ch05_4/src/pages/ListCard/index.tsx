e33b1a2613123f0c1dc3070cc68d01ebdeef11fe
TMInstaller <ajvldmlqjqclr@naver.com>
2023-02-19 07:55:09 +0900

    490p

--- ch05/ch05_4/src/pages/ListCard/index.tsx
+++ ch05/ch05_4/src/pages/ListCard/index.tsx
@@ -1,7 +1,9 @@
 // 413p 사용자 카드 작성하기
 // 463p listCard 컴포넌트 작성하기
+// 490p CardDraggable 컴포넌트 적용하기
 import { FC } from 'react'
 import { ICard } from '../../data'
+import { CardDraggable } from '../../components'
 import { Div, Avatar } from '../../components'
 import { Icon } from '../../theme/daisyui'
 
@@ -9,37 +11,47 @@ export type ListCardProps = {
   card: ICard
   onRemove?: () => void
   onClick?: () => void
+  draggableId: string
+  index: number
 }
 
-const ListCard: FC<ListCardProps> = ({ card, onRemove, onClick }) => {
+const ListCard: FC<ListCardProps> = ({
+  card,
+  onRemove,
+  onClick,
+  draggableId,
+  index,
+}) => {
   const { image, writer } = card
   const { avatar, name, jobTitle } = writer
 
   return (
-    <Div
-      className="m-2 border shadow-lg rounded-xl"
-      width="10rem"
-      onClick={onClick}
-    >
-      <Div src={image} className="relative h-20 ">
-        <Icon
-          name="remove"
-          className="absolute right-1 top-1 btn-primary btn-xs"
-          onClick={onRemove}
-        />
-      </Div>
-      <Div className="flex flex-col p-2 ">
-        <Div minHeight="4rem" height="4rem" maxHeight="4rem">
-          <Div className="flex flex-row items-center ">
-            <Avatar src={avatar} size="2rem" />
-            <Div className="ml-2 ">
-              <p className="text-xs font-bold ">{name}</p>
-              <p className="text-xs text-gray-500 ">{jobTitle}</p>
+    <CardDraggable draggableId={draggableId} index={index}>
+      <Div
+        className="m-2 border shadow-lg rounded-xl"
+        width="10rem"
+        onClick={onClick}
+      >
+        <Div src={image} className="relative h-20 ">
+          <Icon
+            name="remove"
+            className="absolute right-1 top-1 btn-primary btn-xs"
+            onClick={onRemove}
+          />
+        </Div>
+        <Div className="flex flex-col p-2 ">
+          <Div minHeight="4rem" height="4rem" maxHeight="4rem">
+            <Div className="flex flex-row items-center ">
+              <Avatar src={avatar} size="2rem" />
+              <Div className="ml-2 ">
+                <p className="text-xs font-bold ">{name}</p>
+                <p className="text-xs text-gray-500 ">{jobTitle}</p>
+              </Div>
             </Div>
           </Div>
         </Div>
       </Div>
-    </Div>
+    </CardDraggable>
   )
 }
 export default ListCard
