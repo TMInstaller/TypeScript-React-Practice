ad81c34f0b6e1979c067b971ea23b10d0b2c97bf
TMInstaller <ajvldmlqjqclr@naver.com>
2023-02-19 02:45:45 +0900

    463p

--- ch05/ch05_4/src/pages/ListCard/index.tsx
+++ ch05/ch05_4/src/pages/ListCard/index.tsx
@@ -1,9 +1,45 @@
-// 230p 상대 경로 문제 해결하기
-export default function CopyMe() {
+// 413p 사용자 카드 작성하기
+// 463p listCard 컴포넌트 작성하기
+import { FC } from 'react'
+import { ICard } from '../../data'
+import { Div, Avatar } from '../../components'
+import { Icon } from '../../theme/daisyui'
+
+export type ListCardProps = {
+  card: ICard
+  onRemove?: () => void
+  onClick?: () => void
+}
+
+const ListCard: FC<ListCardProps> = ({ card, onRemove, onClick }) => {
+  const { image, writer } = card
+  const { avatar, name, jobTitle } = writer
+
   return (
-    <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center ">CopyMe</h2>
-      <div className="mt-4"></div>
-    </section>
+    <Div
+      className="m-2 border shadow-lg rounded-xl"
+      width="10rem"
+      onClick={onClick}
+    >
+      <Div src={image} className="relative h-20 ">
+        <Icon
+          name="remove"
+          className="absolute right-1 top-1 btn-primary btn-xs"
+          onClick={onRemove}
+        />
+      </Div>
+      <Div className="flex flex-col p-2 ">
+        <Div minHeight="4rem" height="4rem" maxHeight="4rem">
+          <Div className="flex flex-row items-center ">
+            <Avatar src={avatar} size="2rem" />
+            <Div className="ml-2 ">
+              <p className="text-xs font-bold ">{name}</p>
+              <p className="text-xs text-gray-500 ">{jobTitle}</p>
+            </Div>
+          </Div>
+        </Div>
+      </Div>
+    </Div>
   )
 }
+export default ListCard
